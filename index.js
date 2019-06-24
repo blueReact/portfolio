189841d12c6cdb2f76ff68254d76abc158478c75
rahul <rahul_r156@yahoo.com>
2019-06-24 23:42:26 +0530

    added helmet and compression

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const compression = require('compression');
+const helmet = require('helmet');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const router = require('./routes/routes');
+
+
+// compress all responses
+app.use(compression());
+
+app.use(helmet());
 
 // static assets
 app.use(express.static(path.join(__dirname, 'public')));
